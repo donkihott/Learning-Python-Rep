5967a82d0481f8d3e230c28e58a39c8588404c17
donkihott <sudiatjumin@gmail.com>
2026-01-18 18:56:33 +0200

    1. Leaning Fixes #38

--- Past/Algoritms-Programming Practice.py
+++ Past/Algoritms-Programming Practice.py
@@ -1,70 +1,11 @@
-# Lecture 1 "Programming Practice"
-# https://www.youtube.com/watch?v=us7y0UhTq0s&list=PLRDzFCPr95fIDJUvFxvzWxg-V9BmZlMMe&index=3
-s = 'Hello world!!!'
-print(s)
 
-print()
-print('str * 3')
-print(s * 3)
 
-# if you click and select several lines with the mouse, then print it in all lines
-print(5)
-print(5)
 
-# Ctrl + Alt + L - fixes all bugs
-print()
-print('"x" to the expression "5/4"')
-x = 15 / 4  # assign the name "x" to the expression "5/4" (3.75)
-
-y = x  # assign the name "y" to the expression "x"
-print(y)
-
-y == x  # are two names equal? The result is "True" or "False"
-
-x is y  # Is "x" "y"? The result is "True" or "False"
-
-y >= 3  # Comparison "y" is greater than or equal to "3"? The result is "True" or "False"
-
-type(x)  # What is the type of 'y'. Now it is class 'float' (3.75).
-print()
-print('Change type x to class int')
-x = int(x)  # Change type 'x' to class 'int'
-print(x)  # (3)
-
-s = '12345.99'
-print()
-print('Str into the foat. If str is float, it will not correct to change str to int')
-x = float(s)  # If str is float, it will not correct to change str to 'int'
-print(x)  # (12345.99)
-
-print()
-print('float to int - without everything after the "." (Not round)')
-y = int(x)
-print(y)  # (12345) - without everything after the '.' (Not round)
-
-print()
-print('round(12345.99)')
-a = round(12345.99)
-print(a)  # (12346)
-
-# name = input() - The text from Keyboard
-
-name = 'Yura'
-print()
-print('str and function across the "," ')
-print('Привет', name, '!')  # (Привет Yura !)
-
-print()
-print('function into the str')
-print(f'Привет, {name}!')  # (Привет, Yura!) - 'f' before the ' and function in {} - Create function into the str
-
-# x = x - 1 is x -= 1
-print()
 print('formula choose all numbers from "start" (9) to (0) - "stop"')
 x = 9  # start (9)
 while x >= 0:  # choose all numbers from 'start' (9) to (0) - 'stop'
     print(x)
-    x -= 1  # 'step'
+    x -= 1  # 'step', the came: x = x - 1
 
 print()
 print(' cicle for choose numbers that were put')
