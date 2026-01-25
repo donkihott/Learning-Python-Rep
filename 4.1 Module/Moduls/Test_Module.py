86def6060d265e64bac41a44cdec778808262467
donkihott <sudiatjumin@gmail.com>
2026-01-25 20:20:19 +0200

    Theory Fixes #43

---
+++ 4.1 Module/Moduls/Test_Module.py
@@ -0,0 +1,2 @@
+def my_func():
+    return 'Hello, this is the test of connection - from ANOTHER folder!'
--- 4.1 Module/Test_Module.py
+++
@@ -1,4 +0,0 @@
-def my_func():
-    print('Hello, this is the test of connection')
-
-my_func()
