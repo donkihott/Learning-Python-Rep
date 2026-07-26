b6249c182053ab34e7070db7830d64f9e3fb0891
donkihott <sudiatjumin@gmail.com>
2026-07-26 07:05:04 +0300

    genepy 26/07/2026

--- 4.1 Module/Test_Module_near.py
+++ 4.1 Module/Test_Module_near.py
@@ -1,2 +1,11 @@
 def my_func():
     return 'Hello, this is the test of connection - from the SAME folder!'
+
+import pathlib
+import sys
+# 1. Повний шлях до файлу (наприклад: /home/user/project/script.py)
+full_path = sys.argv[0]
+# 2. Отримуємо тільки чисте ім'я файлу (script.py)
+file_name = pathlib.Path(full_path).name
+
+print(f"Ім'я файлу, що виконується: {file_name}")
